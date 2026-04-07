acab738c3818d1db5fb873d5a0881014c7e0e4ba
MarwanGLT <ton-email-github@gmail.com>
2026-04-07 15:23:38 +0200

    Mission#3

--- src/classtp/Voiture.java
+++ src/classtp/Voiture.java
@@ -1,3 +1,5 @@
+package classtp;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +10,8 @@
  * @author marwan
  */
 public class Voiture {
-    
+    String marque;
+    String modele;
+    int annee;
+    double prix;
 }
