0719ea484994f5dc808b125ed4159c012c519c28
MarwanGLT <ton-email-github@gmail.com>
2026-04-13 13:31:02 +0200

    Mission 1

--- src/classtp/ClassTP.java
+++ src/classtp/ClassTP.java
@@ -17,7 +17,7 @@ public class ClassTP {
     Pneu p1 = new Pneu(205, 55, 16, "été");
     Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500, p1);
 
-    System.out.println("Voiture 1 : " + v1.marque + " " + v1.modele + " " + v1.annee + " " + v1.prix +" " + "Hauteur : " + v1.pneu.hauteur +" " + "Largeur : " + v1.pneu.largeur +" " + "Diamètre : " + v1.pneu.diametre +" " + "Type : " + v1.pneu.type);
+    System.out.println("Voiture : " + v1.marque + "\n" + "Modèle : " + v1.modele + "\n" + "Année : " + v1.annee + "\n" + "Prix : " + v1.prix + " €");
 
 }
 
